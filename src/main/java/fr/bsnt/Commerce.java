a97f8e8b7970669026f7b5f67a0af4e0e5a7e08f
Tilak Basnet <tlkbsntnpl@gmail.com>
2023-03-01 08:48:07 +0100

    E-commerce project setup with Dependencies and Database Ecommerce with application.properties file

---
+++ src/main/java/fr/bsnt/Commerce.java
@@ -0,0 +1,12 @@
+package fr.bsnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Commerce {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Commerce.class,args);
+    }
+}
